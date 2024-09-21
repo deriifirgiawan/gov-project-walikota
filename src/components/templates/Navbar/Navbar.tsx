a1edfae9bc4e2a07337a3519f3bea2d779f075ca
deriifirgiawan <derifirgiawan025@gmail.com>
2024-09-21 17:26:49 +0700

    feat: fix style dropdown menu

--- src/components/templates/Navbar/Navbar.tsx
+++ src/components/templates/Navbar/Navbar.tsx
@@ -80,12 +80,13 @@ export const Navbar = () => {
 										</NavigationMenuTrigger>
 									</Link>
 									<NavigationMenuContent>
-										<ul className="w-[400px]">
+										<ul className="w-[400px] p-8 gap-5 flex flex-col">
 											{value.subLink?.map((item) => {
 												return (
 													<Link
 														key={item.slug}
 														href={`/${value.slug}${item.slug}`}
+														className="hover:text-red-700"
 													>
 														{item.name}
 													</Link>
@@ -100,7 +101,7 @@ export const Navbar = () => {
 				})}
 			</div>
 
-			<div className="block md:hidden lg:hidden lg:flex gap-2">
+			<div className="block md:hidden lg:hidden gap-2">
 				<button onClick={() => setOpenSheet(true)}>
 					<Menu className={`${scrolled ? "text-black" : "text-white"}`} />
 				</button>
@@ -159,7 +160,7 @@ export const Navbar = () => {
 												} ml-4`}
 												href={`/${item.slug}`}
 											>
-												- {item.name}
+												{item.name}
 											</Link>
 										))}
 									</div>
